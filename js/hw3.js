ef6a2d5f8c0607b809f6460d2bcea5bf780c133c
Viktor Kononov <viktor1998kononov@gmail.com>
2023-11-18 00:33:33 +0200

    completed hw3

---
+++ js/hw3.js
@@ -0,0 +1,12 @@
+const firstString = prompt("Enter first string:");
+const secondString = prompt("Enter second string:");
+const thirdString = prompt("Enter third string:");
+
+document.querySelector('#concat').innerHTML = firstString + secondString + thirdString;
+
+const x = 10000;
+const xs = x.toString();
+const xS = `${xs[0]} ${xs[1]} ${xs[2]} ${xs[3]} ${xs[4]}`;
+document.querySelector("#number1").innerHTML = xS;
+
+
