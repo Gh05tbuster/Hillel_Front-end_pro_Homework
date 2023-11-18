c383d924230a80ca3dc9c3f9769ff1f71383b2c8
Viktor Kononov <viktor1998kononov@gmail.com>
2023-11-18 14:27:07 +0200

    hw4 fix

--- js/hw4.js
+++ js/hw4.js
@@ -1,7 +1,13 @@
 const a = prompt("Enter first number:");
 const b = prompt("Enter second number:");
+alert(`
+• ${a} + ${b} = ${+a + +b} \n
+• ${a} - ${b} = ${+a - +b} \n
+• ${a} * ${b} = ${+a * +b} \n
+• ${a} / ${b} = ${+a / +b}
+`);
 
-document.querySelector('#addition').innerHTML = `${a} + ${b} = ${+a + +b}`;
-document.querySelector('#substraction').innerHTML = `${a} - ${b} = ${+a - +b}`;
-document.querySelector('#multiplication').innerHTML = `${a} * ${b} = ${+a * +b}`;
-document.querySelector('#division').innerHTML = `${a} / ${b} = ${+a / +b}`;
+// document.querySelector('#addition').innerHTML = `${a} + ${b} = ${+a + +b}`;
+// document.querySelector('#substraction').innerHTML = `${a} - ${b} = ${+a - +b}`;
+// document.querySelector('#multiplication').innerHTML = `${a} * ${b} = ${+a * +b}`;
+// document.querySelector('#division').innerHTML = `${a} / ${b} = ${+a / +b}`;
