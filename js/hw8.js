7d318695d74e9e318fbbdfee55ae76551b253a2b
Viktor Kononov <viktor1998kononov@gmail.com>
2023-11-24 21:38:12 +0200

    completed hw8

---
+++ js/hw8.js
@@ -0,0 +1,7 @@
+const a = prompt("Enter first number:");
+const b = prompt("Enter second number:");
+const c = prompt("Enter third number:");
+
+const average = (+a + +b + +c) / 3;
+
+alert(`Average: ${average}`);
