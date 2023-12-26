41dd69fd583cf205f755a3b895a0d770222cc360
Viktor Kononov <viktor1998kononov@gmail.com>
2023-12-26 14:48:18 +0200

    completed hw22

--- js/hw22.js
+++ js/hw22.js
@@ -1,12 +1,20 @@
 class Hamburger {
+    hamburger = {
+        price: 0,
+        calValue: 0,
+    }
+
     constructor(size, stuffing) {
+        this.hamburger.price += size.price;
+        this.hamburger.calValue += size.calValue;
+        this.hamburger.price += stuffing.price;
+        this.hamburger.calValue += stuffing.calValue;
     }
 
     static sizeSmall = {
         price: 50,
         calValue: 20,
     }
-
     static sizeBig = {
         price: 100,
         calValue: 40,
@@ -29,21 +37,30 @@ class Hamburger {
         price: 15,
         calValue: 0,
     }
-
     static toppingMayo = {
         price: 20,
         calValue: 5,
     }
 
     addTopping(topping) {
-
+        this.hamburger.price += topping.price;
+        this.hamburger.calValue += topping.calValue;
     }
 
     calculateCalories() {
-
+        return this.hamburger.calValue;
     }
 
     calculatePrice() {
-
+        return this.hamburger.price;
     }
 }
+
+let hamburger = new Hamburger(Hamburger.sizeBig, Hamburger.stuffingCheese);
+
+hamburger.addTopping(Hamburger.toppingMayo);
+console.log(`Price: ${hamburger.calculatePrice()}`);
+console.log(`Calories: ${hamburger.calculateCalories()}`);
+
+hamburger.addTopping(Hamburger.toppingSpice);
+console.log(`Price: ${hamburger.calculatePrice()}`);
