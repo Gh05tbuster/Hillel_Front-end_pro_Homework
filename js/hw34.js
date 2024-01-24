a6db6eea7f2ad77eaab6f9d25af0c56b6d53c3af
Viktor Kononov <viktor1998kononov@gmail.com>
2024-01-24 18:34:53 +0200

    completed hw34

---
+++ js/hw34.js
@@ -0,0 +1,9 @@
+const emotes = document.querySelector('#rateBox .emotes');
+emotes.addEventListener('click', vote);
+
+function vote(event) {
+    const target = event.target;
+    if (target.tagName != 'IMG') return;
+    const count = document.querySelector(`#rateBox .emotes #${target.id} + .count`);
+    count.innerHTML = +count.innerHTML + 1;
+}
