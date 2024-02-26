af8a44cabbb0f2d5588bac2d09b10a1d9fae1d86
Viktor Kononov <viktor1998kononov@gmail.com>
2024-02-26 18:31:00 +0200

    added dots

--- js/slider_basic.js
+++ js/slider_basic.js
@@ -1,41 +1,44 @@
 const numberOfImages = 7;
-
 const imageBox = document.querySelector('.basic-image-slider .image-box');
-for (let i = 1; i <= numberOfImages; i++) {
-    imageBox.innerHTML += `
-    <img src="./img/laptop/${i}.webp" alt="" class="carousel-image" id="ci_${i - 1}">`;
-}
+const dots = document.querySelector('.basic-image-slider .basic-slider-dots');
+dots.addEventListener('click', (event) => {
+    const classes = event.target.classList;
+    if (!classes.contains('dot')) return;
+    const dotId = event.target.id.split('_')[1];
+    swipeOn(dotId - 1);
+})
 
+createCarousel(numberOfImages);
 const carouselImages = document.querySelectorAll('.basic-image-slider .image-box .carousel-image');
-carouselImages.forEach(image => image.classList.add('invisible'));
 let activeImg = 0;
-showRelevantImages(carouselImages);
+let activeDot = document.getElementById(`dot_${activeImg + 1}`);
+activeDot.classList.add('active');
 
 const prevBtn = document.querySelector('.basic-image-slider .prev');
-prevBtn.addEventListener('click', () => swipeOn(-1));
+prevBtn.addEventListener('click', () => swipeOn(activeImg - 1));
 const nextBtn = document.querySelector('.basic-image-slider .next');
-nextBtn.addEventListener('click', () => swipeOn(1));
-showHideButtons();
+nextBtn.addEventListener('click', () => swipeOn(activeImg + 1));
+showAndHideArrows();
 
-function swipeOn(tmp) {
-    activeImg += tmp;
+function createCarousel(n) {
+    for (let i = 1; i <= n; i++) {
+        imageBox.innerHTML += `
+        <img src="./img/laptop/${i}.webp" alt="" class="carousel-image" id="ci_${i - 1}">`;
+        dots.innerHTML += `
+        <div class="dot" id="dot_${i}"></div>`;
+    }
+}
+
+function swipeOn(pos) {
+    activeImg = pos;
     carouselImages.forEach(image => {
         image.style.transform = `translateX(calc(-100% * ${activeImg} - 30px * ${activeImg}))`;
     });
-    showRelevantImages(carouselImages);
-    showHideButtons();
+    showAndHideArrows();
+    swapActiveDot();
 }
 
-function showRelevantImages(carouselImages) {
-    carouselImages.forEach(image => image.classList.add('invisible'));
-    if (activeImg > 0)
-        carouselImages[activeImg - 1].classList.remove('invisible');
-    carouselImages[activeImg].classList.remove('invisible');
-    if (activeImg < numberOfImages - 1)
-        carouselImages[activeImg + 1].classList.remove('invisible');
-}
-
-function showHideButtons() {
+function showAndHideArrows() {
     if (activeImg === 0) {
         prevBtn.classList.add('invisible');
         prevBtn.style.pointerEvents = 'none';
@@ -53,4 +56,8 @@ function showHideButtons() {
     }
 }
 
-console.log('');
+function swapActiveDot() {
+    activeDot.classList.remove('active');
+    activeDot = document.getElementById(`dot_${activeImg + 1}`);
+    activeDot.classList.add('active');
+}
