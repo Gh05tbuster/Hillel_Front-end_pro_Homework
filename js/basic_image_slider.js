7632777f117efb595b4cce4bb64bc8744b75b794
Viktor Kononov <viktor1998kononov@gmail.com>
2024-02-26 23:12:00 +0200

    fixed dots position and numeration

--- js/basic_image_slider.js
+++ js/basic_image_slider.js
@@ -4,26 +4,26 @@ const dots = document.querySelector('.basic-image-slider .basic-slider-dots');
 dots.addEventListener('click', (event) => {
     const classes = event.target.classList;
     if (!classes.contains('dot')) return;
-    const dotId = event.target.id.split('_')[1];
-    swipeOn(dotId - 1);
+    const dotId = +event.target.id.split('_')[1];
+    swipeOn(dotId);
 })
 
 createCarousel(numberOfImages);
 const carouselImages = document.querySelectorAll('.basic-image-slider .image-box .carousel-image');
 let activeImg = 0;
-let activeDot = document.getElementById(`dot_${activeImg + 1}`);
+let activeDot = document.getElementById(`dot_${activeImg}`);
 activeDot.classList.add('active');
 
 const prevBtn = document.querySelector('.basic-image-slider .prev');
 prevBtn.addEventListener('click', () => swipeOn(activeImg - 1));
 const nextBtn = document.querySelector('.basic-image-slider .next');
 nextBtn.addEventListener('click', () => swipeOn(activeImg + 1));
-showAndHideArrows();
+showAndHideArrows(activeImg);
 
 function createCarousel(n) {
-    for (let i = 1; i <= n; i++) {
+    for (let i = 0; i < n; i++) {
         imageBox.innerHTML += `
-        <img src="./img/laptop/${i}.webp" alt="" class="carousel-image" id="ci_${i - 1}">`;
+        <img src="./img/laptop/${i + 1}.webp" alt="" class="carousel-image" id="ci_${i}">`;
         dots.innerHTML += `
         <div class="dot" id="dot_${i}"></div>`;
     }
@@ -34,12 +34,12 @@ function swipeOn(pos) {
     carouselImages.forEach(image => {
         image.style.transform = `translateX(calc(-100% * ${activeImg} - 30px * ${activeImg}))`;
     });
-    showAndHideArrows();
+    showAndHideArrows(pos);
     swapActiveDot();
 }
 
-function showAndHideArrows() {
-    if (activeImg === 0) {
+function showAndHideArrows(pos) {
+    if (pos === 0) {
         prevBtn.classList.add('invisible');
         prevBtn.style.pointerEvents = 'none';
     } else {
@@ -47,7 +47,7 @@ function showAndHideArrows() {
         prevBtn.style.pointerEvents = 'auto';
     }
 
-    if (activeImg === numberOfImages - 1) {
+    if (pos === numberOfImages - 1) {
         nextBtn.classList.add('invisible');
         nextBtn.style.pointerEvents = 'none';
     } else {
@@ -58,6 +58,6 @@ function showAndHideArrows() {
 
 function swapActiveDot() {
     activeDot.classList.remove('active');
-    activeDot = document.getElementById(`dot_${activeImg + 1}`);
+    activeDot = document.getElementById(`dot_${activeImg}`);
     activeDot.classList.add('active');
 }
